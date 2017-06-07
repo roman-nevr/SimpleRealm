25916386e1a2e218a8fc58bd351b172932a8fd82
roman-nevr <roman-nevr@yandex.ru>
2017-06-07 11:45:15 +0300

    Initial commit

---
+++ app/src/main/java/org/berendeev/roma/simplerealm/presentation/ui/adapter/FragmentsPagerAdapter.java
@@ -0,0 +1,41 @@
+package org.berendeev.roma.simplerealm.presentation.ui.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import org.berendeev.roma.simplerealm.presentation.ui.fragment.InputFragment;
+
+import java.util.List;
+
+public class FragmentsPagerAdapter extends FragmentPagerAdapter {
+
+    private List<String> titles;
+
+    public FragmentsPagerAdapter(FragmentManager fm, List<String> titles) {
+        super(fm);
+        this.titles = titles;
+    }
+
+    @Override public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:{
+                return InputFragment.getInstance();
+            }
+            case 1:{
+                return InputFragment.getInstance();
+            }
+            default:
+                throw new IllegalArgumentException("illegal viewPager position");
+        }
+    }
+
+    @Override public int getCount() {
+        return titles.size();
+    }
+
+    @Override public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+}
---
+++ app/src/main/java/org/berendeev/roma/simplerealm/presentation/ui/fragment/InputFragment.java
@@ -0,0 +1,9 @@
+package org.berendeev.roma.simplerealm.presentation.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class InputFragment extends Fragment {
+    public static Fragment getInstance() {
+        return null;
+    }
+}
